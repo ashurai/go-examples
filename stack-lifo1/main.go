f04efbbe83d0a56b601b9efa335f3184affaf73d
Ashutosh Rai <dev.ashurai@gmail.com>
2022-02-15 13:42:33 +0530

    adding first GoCode simple example on stack(lifo) implementation without pop output printing

---
+++ stack-lifo1/main.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+type stack []string
+
+func main() {
+	var s stack
+	s.push("My")
+	s.push("name")
+	s.push("is")
+	s.push("Ashutosh")
+
+	fmt.Println(s)
+	s.pop()
+	fmt.Println(s)
+}
+
+func (s *stack) push(s1 string) {
+	*s = append(*s, s1)
+}
+
+func (s *stack) pop() {
+	if len(*s) == 0 {
+		fmt.Println("Empty list")
+	}
+	*s = (*s)[:len(*s)-1]
+}
